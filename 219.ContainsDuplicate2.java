8598857157e0eb3f99a916fb7da446f00b68bbeb
TheVandeleuriaCypher <146964548+TheVandeleuriaCypher@users.noreply.github.com>
2024-04-06 13:41:52 +0100

    Create 219.ContainsDuplicate2.java

---
+++ 219.ContainsDuplicate2.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map <Integer,Integer> numMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++){
+            if (numMap.containsKey(nums[i]) && (i - numMap.get(nums[i]) <= k))
+                return true;
+            numMap.put(nums[i],i);
+        }
+        return false;
+    }
+}
